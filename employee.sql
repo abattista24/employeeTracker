66ed7b70c345ec11d32a5cb49b57dd417ec1f18a
Anna Battistals <anna.r.battista@gmail.com>
2020-10-29 22:33:10 -0600

    added psydo code for index.js inquire prompts

--- employee.sql
+++ employee.sql
@@ -4,23 +4,34 @@ USE staff_db;
 
 --create tables
 
-CREATE TABLE employee(
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER (10),
-    manager_id INTEGER (10)
+
+CREATE TABLE departments (
+    id INTEGER (10) AUTO_INCREMENT NOT NULL,
+    name VARCHAR (30),
+    PRIMARY KEY (id),
 );
 
 
 CREATE TABLE role(
+    id INTEGER (10) AUTO_INCREMENT NOT NULL,
     title VARCHAR (30),
-    salary INTEGER (10),
-    department_id INTEGER (10)
+    salary DECIMAL (10),
+    --use department id from above
+    department_id INTEGER (10), 
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE department (
-    name VARCHAR (30)
+
+CREATE TABLE employee(
+    id INTEGER (10) AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    --use role id from above
+    role_id INTEGER (10),
+    manager_id INTEGER (10)
+    PRIMARY KEY (id)
 );
+
 
 --put data into tables
 
