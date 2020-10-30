9fc50d95b96e11d5f226e1e44ae07d036ed37d35
Anna Battistals <anna.r.battista@gmail.com>
2020-10-29 22:08:31 -0600

    fixed tables

--- employee.sql
+++ employee.sql
@@ -1,13 +1,29 @@
 DROP DATABASE IF EXISTS staff_db;
 CREATE DATABASE staff_db;
 USE staff_db;
+
+--create tables
+
 CREATE TABLE employee(
-    first_name VARCHAR(30) NOT NULL;
-    last_name VARCHAR(30) NOT NULL;
-    role_id INTEGER (10);
-    manager_id INTEGER (10);
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER (10),
+    manager_id INTEGER (10)
+);
+
 
 CREATE TABLE role(
-    title VARCHAR(30);
-    
-)
+    title VARCHAR (30),
+    salary INTEGER (10),
+    department_id INTEGER (10)
+);
+
+CREATE TABLE department (
+    name VARCHAR (30)
+);
+
+--put data into tables
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("John", "Doe", 01,01);
+
